521a16fd6369b3b765fb6a21a7ee80611e88bc6b
Coding Padawan <gustavohiguera9715@gmail.com>
2023-07-06 20:17:34 -0700

    worked on the checkAnswer function

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
 var gamePattern = [];
 var gameLevel = 0;
+var started = false;
 
-$(document).on("keypress", nextSequence);
+$(document).on("keypress", function () {
+  if (!started) {
+    $("#level-title").text("Level " + gameLevel);
+    nextSequence();
+    started = true;
+  }
+});
 
 function nextSequence() {
+  //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+
+  userClickedPattern = [];
+
+  gameLevel++;
 
   $("#level-title").text("Level " + gameLevel);
 
@@ -20,26 +32,26 @@ function nextSequence() {
 }
 
 $(".btn").on("click", function (e) {
-  var userChosenColour = e.target.id;
-  var userChosenColourBtn = e.target;
+  var userChosenColour = $(this).attr("id");
 
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
-  animatePress(userChosenColourBtn);
+  animatePress(userChosenColour);
   checkAnswer(userClickedPattern.length - 1);
 });
+
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-function animatePress(currentColour) {
-  $(currentColour).addClass("pressed");
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
 
   setTimeout(function () {
-    $(currentColour).removeClass("pressed");
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
 
@@ -49,6 +61,14 @@ function checkAnswer(currentLevel) {
 
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     console.log("correct");
+
+    if (userClickedPattern.length === gamePattern.length) {
+
+      setTimeout(function () {
+        nextSequence();
+
+      }, 1000);
+    }
 
   } else {
     console.log("Incorrect");
